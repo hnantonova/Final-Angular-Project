dd8ca4b4f69e1edc39ad393fc37457b00ab4a8f9
hnantonova <hristina.n.antonova@gmail.com>
2024-12-05 09:30:57 +0200

    update dialog with a reactive form

--- src/app/components/dialog-create-post/dialog-create-post.component.ts
+++ src/app/components/dialog-create-post/dialog-create-post.component.ts
@@ -1,24 +1,61 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { collectionTypes } from '../../models/collection-types.enum';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FirestoreService } from '../../firestore/firestore.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-create-post',
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule],
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './dialog-create-post.component.html',
   styleUrl: './dialog-create-post.component.scss',
 })
 export class DialogCreatePostComponent {
   method: any = '';
-  ngOnInit() {
+  myForm: FormGroup;
+
+  constructor(
+    private firestoreService: FirestoreService,
+    private fb: FormBuilder
+  ) {
     this.method = inject(MAT_DIALOG_DATA);
+
+    this.myForm = this.fb.group({
+      title: ['', [Validators.required]],
+      body: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {
     console.log(this.method);
   }
 
   createPost() {
+    const { title, body } = this.myForm.value;
+
+
+    const res = this.firestoreService.createNewPost(title, body);
+
+    console.log({ res });
+
     if (this.method === collectionTypes.MomsCollection) {
       // send req to push in collection formoms
+      console.log('create post');
     }
     if (this.method === collectionTypes.TravelCollection) {
       // send req to push in collection travel
