185a27662de6c54a9d253401f3fcce4e3cbd19cf
hnantonova <hristina.n.antonova@gmail.com>
2024-12-18 00:27:37 +0200

    add page not found component

---
+++ src/app/components/page.not.found/page.not.found.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page.not.found',
+  standalone: true,
+  imports: [RouterModule, MatButtonModule],
+  templateUrl: './page.not.found.component.html',
+  styleUrl: './page.not.found.component.scss'
+})
+export class PageNotFoundComponent {
+
+}
